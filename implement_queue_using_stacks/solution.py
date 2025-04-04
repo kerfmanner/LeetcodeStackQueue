b50132865d359c185a226bcde9cd9f23c33dd9c8
Luka Konovalov <konovluka@gmail.com>
2025-04-04 16:45:28 +0300

    Implemented queue using two stacks without amortization, also added screenshot of passing tests.

---
+++ implement_queue_using_stacks/solution.py
@@ -0,0 +1,79 @@
+'''Implement Queue using Stacks'''        
+class Node:
+    '''A node class for linked list implementation.'''
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class StackLinkedList:
+    '''A simple stack implementation using a linked list.'''
+    def __init__(self):
+        self.top = None
+        self.size = 0
+
+    def is_empty(self):
+        '''Check if the stack is empty.'''
+        return self.size == 0
+
+    def push(self, item):
+        '''Push an item onto the stack.'''
+        new_node = Node(item)
+        new_node.next = self.top
+        self.top = new_node
+        self.size += 1
+
+    def pop(self):
+        '''Return the top item of the stack and remove it."""'''
+        if self.is_empty():
+            raise IndexError("pop from empty stack")
+        popped_node = self.top
+        self.top = self.top.next
+        self.size -= 1
+        return popped_node.data
+
+    def peek(self):
+        '''Return the top item of the stack without removing it."""'''
+        if self.is_empty():
+            raise IndexError("peek from empty stack")
+        return self.top.data
+
+class MyQueue:
+
+    def __init__(self):
+        self.mainstack = StackLinkedList()
+        self.storestack = StackLinkedList()
+
+    def push(self, x: int) -> None:
+        self.mainstack.push(x)
+
+    def pop(self) -> int:
+        if self.empty():
+            return None
+
+        while not self.empty():
+            element = self.mainstack.pop()
+            self.storestack.push(element)
+        first = self.storestack.pop()
+
+        while not self.storestack.is_empty():
+            element = self.storestack.pop()
+            self.mainstack.push(element)
+        return first
+
+    def peek(self) -> int:
+        if self.empty():
+            return None
+
+        while not self.empty():
+            element = self.mainstack.pop()
+            self.storestack.push(element)
+        first = self.storestack.peek()
+
+        while not self.storestack.is_empty():
+            element = self.storestack.pop()
+            self.mainstack.push(element)
+        return first
+        
+
+    def empty(self) -> bool:
+        return self.mainstack.is_empty()
